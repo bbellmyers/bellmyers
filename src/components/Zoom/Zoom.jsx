e172400f1072c0f0e27c43b396ece1e6963e2a8a
Bruce Bell-Myers <bbellmyers@ptc.com>
2018-11-22 19:06:04 -0600

    zoom fit; speedy menu

--- src/components/Zoom/Zoom.jsx
+++ src/components/Zoom/Zoom.jsx
@@ -1,67 +1,69 @@
 import React, { Component } from 'react';
 import './Zoom.css';
 
+const SPACER = 'images/spacer.gif';
+
 class Zoom extends Component {
 
-	constructor() {
-		super();
-		this.state = {
-			zoomed: false,
-			zoomSrc: 'images/spacer.gif'
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      zoomed: false,
+      zoomSrc: SPACER,
+      zoomWidth: 0,
+      zoomHeight: 0
+    };
+  }
 
   render() {
     return (
-			<button id="shadow" className={this.state.zoomed? "" : "closed"} onClick={() => this.closeShadow()}>
-				<table>
-						<tbody>
-						<tr>
-								<td>
-									<img border="0" id="zoomZoomImage" src={this.state.zoomSrc} alt="(c) copyright Darcy Bell-Myers" />
-								</td>
-						</tr>
-						<tr className="caption">
-							<h2>{this.props.sample.title}</h2>
-							{this.props.sample.subtitle}
-						</tr>
-						</tbody>
-				</table>
-			</button>
-		);
-	}
+      <button id="shadow" className={this.state.zoomed? "" : "closed"} onClick={() => this.closeShadow()}>
+        <table className="zoomPanel" style={{width: this.state.zoomWidth, height: this.state.zoomHeight}}>
+            <tbody>
+            <tr>
+                <td>
+                  <img border="0" id="zoomZoomImage" src={this.state.zoomSrc} alt="(c) copyright Darcy Bell-Myers" />
+                </td>
+            </tr>
+            </tbody>
+        </table>
+      </button>
+    );
+  }
 
-	closeShadow() {
+  closeShadow() {
     this.setState({
       zoomed: false
     });
-	}
+  }
 
-	componentWillReceiveProps(props) {
-		if (props.sample.full) {
-			this.setState({
-				zoomSrc: props.sample.full ? props.sample.full : 'images/spacer.gif'
-			});
-			var zoom = document.getElementById("zoomZoomImage");
-			zoom.style.width = null;
-			zoom.style.height = null;
-			zoom.addEventListener("load", () => {
-				if (zoom.width > window.innerWidth) {
-						var reducedWidth = window.innerWidth - 30 + "px";
-						zoom.style.width = reducedWidth;
-				}
-				// still too big
-				if (zoom.height > window.innerHeight) {
-						zoom.style.width = null;
-						var reducedHeight = window.innerHeight - 30 + "px";
-						zoom.style.height = reducedHeight;
-				}
-				this.setState({
-					zoomed: true
-				});
-			});
-		}
-	}
+  componentWillReceiveProps(props) {
+    if (props.sample.full) {
+      this.setState({
+        zoomSrc: props.sample.full ? props.sample.full : SPACER
+      });
+      var zoom = document.getElementById("zoomZoomImage");
+      zoom.addEventListener("load", () => {
+        if (zoom.src.indexOf(SPACER) < 0) {
+          let ratio = zoom.width / zoom.height;
+
+          let reducedWidth = Math.min(zoom.width, window.innerWidth - 48);
+          let reducedHeight = reducedWidth / ratio;
+
+          if (reducedHeight > window.innerHeight - 48) {
+            reducedHeight = window.innerHeight - 48;
+            reducedWidth = reducedHeight * ratio;
+          }
+
+          this.setState({
+            zoomed: true,
+            zoomWidth: reducedWidth,
+            zoomHeight: reducedHeight
+          });
+        }
+      });
+    }
+  }
 
 }
 
